1b49b42d60ea7f937ca6df98ca5dc4f2bdc8e710
sampleboy <1025983858@qq.com>
2021-11-19 10:11:22 +0800

    所有代码

--- mapreduce/src/main/java/com/atguigu/mr/wordCount/Driver.java
+++ mapreduce/src/main/java/com/atguigu/mr/wordCount/Driver.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 
 
 public class Driver {
+
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 //        1.获取job实例；
         Job job = Job.getInstance(new Configuration());
